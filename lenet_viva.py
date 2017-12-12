9833c1214a58c0a4307fd5cca5ae7f7bf84e773a
Kyle Yoon <apps.yoon@gmail.com>
2017-12-12 07:52:27 -0600

    line

--- lenet_viva.py
+++ lenet_viva.py
@@ -43,9 +43,10 @@ train_img_list, train_box_list, test_img_list, test_box_list = load_viva(path)
 # Splitting into train validation 75/25
 train_img_list, valid_img_list, train_box_list, valid_box_list = train_test_split(
     train_img_list, train_box_list, test_size=0.25)
-print('[INFO] Loaded {} training data'.format(len(train_img_list)))
-print('[INFO] Loaded {} validation data'.format(len(valid_img_list)))
-print('[INFO] Loaded {} testing data'.format(len(test_img_list)))
+print('[INFO] Loaded {} training images'.format(len(train_img_list)))
+print('[INFO] Loaded {} validation images'.format(len(valid_img_list)))
+print('[INFO] Loaded {} testing images'.format(len(test_img_list)))
+
 
 """
 Model Compilation
